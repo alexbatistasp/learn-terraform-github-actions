fb5e72dfabf7a57479dd27120c146c45d6dfce65
alex <alex@localhost.localdomain>
2022-02-18 01:44:37 -0800

    add default vpc teste8

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "vpc_public_subnets" {
   value       = module.vpc.public_subnets
 }
 
-output "ec2-dns" {    
-  description = "Public DNS addresses of EC2 instances"  
-  value = module.ec2.public_dns
+output "ec2-dns" {
+  description = "Public DNS addresses of EC2 instances"
+  value       = module.ec2.public_dns
 }
